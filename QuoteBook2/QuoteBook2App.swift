5fad1d0c03fe1e202343c42d6ca2adb2fff2a314
adamTab <tabievadam@gmail.com>
2023-04-27 12:40:40 +0200

    Initial Commit

---
+++ QuoteBook2/QuoteBook2App.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteBook2App.swift
+//  QuoteBook2
+//
+//  Created by Адам Табиев on 27.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct QuoteBook2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
